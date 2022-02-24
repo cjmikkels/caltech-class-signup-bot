80e8469a6a343925f22649243ec282ad8dfe538b
shreshth <shreshthsrivastava2@gmail.com>
2022-02-23 20:35:51 -0800

    Refactor for cleanup

--- caltechClassSignupBot.js
+++ caltechClassSignupBot.js
@@ -1,44 +1,18 @@
 const puppeteer = require('puppeteer');
+const {
+  HTMLtag,
+  clickElementWithCertainText,
+  typeIntoElement,
+} = require('./utils');
+
 require('dotenv').config();
 
+const config = {
+  canViewBrowser: false,
+  delayInMilliseconds: 100,
+};
+
 const { USERNAME, PASSWORD } = process.env;
-
-class HTMLtag {
-  static A = new HTMLtag('a');
-  static Button = new HTMLtag('button');
-
-  constructor(name) {
-    this.name = name;
-  }
-}
-
-/**
- *
- * @param {Page} page
- * @param {string} text - the text of the item you want to click
- * @param {HTMLtag} tagType – which HTML tag type you're clicking, e.g. button, a, etc.
- * @returns
- */
-const clickElementWithCertainText = async (page, text, tagType) => {
-  switch (tagType) {
-    case HTMLtag.A:
-      const [a] = await page.$x(`//a[contains(., '${text}')]`);
-      if (a) {
-        await a.click();
-      }
-      break;
-
-    case HTMLtag.button:
-      break;
-  }
-
-  return;
-};
-
-const typeIntoElement = async (page, elementIdentifier, stringToType) => {
-  await page.focus(elementIdentifier);
-  await page.keyboard.type(stringToType);
-};
 
 const signIn = async (page) => {
   await typeIntoElement(page, 'input[name=login]', USERNAME);
@@ -48,15 +22,19 @@ const signIn = async (page) => {
 };
 
 const signupForClasses = async () => {
-  const browser = await puppeteer.launch({ headless: false, slowMo: 100 });
+  const browser = await puppeteer.launch({
+    headless: !config.canViewBrowser,
+    slowMo: config.delayInMilliseconds,
+  });
 
   const page = await browser.newPage();
-  await page.goto('https://google.com');
+  await page.goto('https://access.caltech.edu');
 
-  // await signIn(page);
+  await signIn(page);
 
-  await clickElementWithCertainText(page, 'Sign in', HTMLtag.A);
+  // await clickElementWithCertainText(page, 'Sign in', HTMLtag.A);
+
+  // await browser.close();
 };
 
-// console.log(USERNAME);
 signupForClasses();
---
+++ utils.js
@@ -0,0 +1,44 @@
+class HTMLtag {
+  static A = new HTMLtag('a');
+  static Button = new HTMLtag('button');
+
+  constructor(name) {
+    this.name = name;
+  }
+}
+
+/**
+ *
+ * @param {Page} page
+ * @param {string} text - the text of the item you want to click
+ * @param {HTMLtag} tagType – which HTML tag type you're clicking, e.g. button, a, etc.
+ * @returns
+ */
+const clickElementWithCertainText = async (page, text, tagType) => {
+  switch (tagType) {
+    case HTMLtag.A:
+      const [a] = await page.$x(`//a[contains(., '${text}')]`);
+      if (a) {
+        await a.click();
+      }
+      break;
+
+    case HTMLtag.button:
+      break;
+  }
+
+  return;
+};
+
+/**
+ *
+ * @param {*} page
+ * @param {*} elementIdentifier - identifier of element to type into e.g. 'input[name=login]'
+ * @param {*} stringToType - string to type
+ */
+const typeIntoElement = async (page, elementIdentifier, stringToType) => {
+  await page.focus(elementIdentifier);
+  await page.keyboard.type(stringToType);
+};
+
+module.exports = { HTMLtag, clickElementWithCertainText, typeIntoElement };
